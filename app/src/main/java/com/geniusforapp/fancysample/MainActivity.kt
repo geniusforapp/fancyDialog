f6ed0241cefef307d569ed2dc9b379953418b910
Ahmad najar <ahmed.najar@trycarriage.com>
2019-04-24 14:14:55 +0300

    just uopdate some resources

--- app/src/main/java/com/geniusforapp/fancysample/MainActivity.kt
+++ app/src/main/java/com/geniusforapp/fancysample/MainActivity.kt
@@ -7,13 +7,16 @@ import android.view.Gravity.START
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.geniusforapp.fancydialog.builders.FancyDialogBuilder
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import com.geniusforapp.fancydialog.builders.fancy
 import com.geniusforapp.fancydialog.interfaces.OnActionClickedListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnActionClickedListener {
 
+    val carsRepo = CarsRepo.newInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,32 +24,35 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         fab.setOnClickListener {
             buildAndShowDialog()
+            carsRepo.addCar("test")
         }
+
+
     }
 
     private fun buildAndShowDialog() {
-        val dialog = FancyDialogBuilder(this)
-                .withCanCancel(false)
-                .withTitleTypeFace(R.font.roboto_bold)
-                .withSubTitleTypeFace(R.font.roboto_medium)
-                .withActionPositiveTypeFace(R.font.roboto)
-                .withActionNegativeTypeFace(R.font.roboto)
-                .withTextGravity(START)
-                .withPanelGravity(END)
-                .withTitle(R.string.dialog_text_title)
-                .withSubTitle(R.string.dialog_text_message)
-                .withPositive(R.string.dialog_action_yes, object : OnActionClickedListener {
-                    override fun onClick(view: View, dialog: Dialog) {
-                        Toast.makeText(this@MainActivity, "Why Me ?", Toast.LENGTH_LONG).show()
-                    }
-                })
-                .withNegative(R.string.dialog_action_no, object : OnActionClickedListener {
-                    override fun onClick(view: View, dialog: Dialog) {
-                        dialog.dismiss()
-
-                    }
-                })
-        dialog.show()
+        fancy(this) {
+            imageDrawable = ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_picture)
+            isCancelable = true
+            titleTypeFace = ResourcesCompat.getFont(this@MainActivity, R.font.roboto_bold)
+            subTitleTypeFace = ResourcesCompat.getFont(this@MainActivity, R.font.roboto_medium)
+            actionPositiveTypeFace = ResourcesCompat.getFont(this@MainActivity, R.font.roboto)
+            actionNegativeTypeFace = ResourcesCompat.getFont(this@MainActivity, R.font.roboto)
+            textGravity = START
+            panelGravity = END
+            title = getString(R.string.dialog_text_title)
+            subTitle = getString(R.string.dialog_text_message)
+            actionPositive = getString(R.string.dialog_action_yes)
+            actionNegative = getString(R.string.dialog_action_no)
+            onActionPositiveClicked = this@MainActivity
+            onActionNegativeClicked = this@MainActivity
+        }.show()
     }
+
+    override fun onClick(view: View, dialog: Dialog) {
+        dialog.dismiss()
+    }
+
+
 }
 
